e58309d4d24d5ae5aa4edd60ebfba80688d1ec94
Marcelo <carrillomarcelo2009@gmail.com>
2025-02-04 11:21:50 -0800

    2/3 changes

--- Pltw 1.2/1.2.4/maze.py
+++ Pltw 1.2/1.2.4/maze.py
@@ -1,43 +1,55 @@
 import turtle
+import random
 
 screen = turtle.Screen()
 mazePainter = turtle.Turtle()
 mazePainter.width(5)
-mazePainter.speed(1)
+mazePainter.speed(5)
 
 MAZE_SIZE = 15
 MAZE_WIDTH = 35
 DOOR_SIZE = 35
 
-def makeDoor(index):
-    mazePainter.forward(MAZE_WIDTH/2 * index)
-    mazePainter.penup()
-    mazePainter.forward(DOOR_SIZE)
-    mazePainter.pendown()
-    mazePainter.forward(MAZE_WIDTH/2 * index - DOOR_SIZE)
-    print(MAZE_WIDTH/2 * index - DOOR_SIZE)
+def makeWall(index):
+    mazePainter.forward(MAZE_WIDTH * index)
 
-def makeBarrier(index):
+def makeDoor(index):
+    position = random.randint(-MAZE_WIDTH, MAZE_WIDTH) * index /2.1
+    splitWallSize = MAZE_WIDTH/2 * index - DOOR_SIZE/2
+
+
+    mazePainter.forward(splitWallSize + position) #make part of wall
     mazePainter.penup()
-    mazePainter.backward((MAZE_WIDTH/2 * index) + 35)
+    mazePainter.forward(DOOR_SIZE) #make the door
+    mazePainter.pendown()
+    mazePainter.forward(splitWallSize - position) #make other part of wall
+
+def makeBarrier(index, withDoor):
+    if withDoor:
+        makeDoor(index)
+    else:    
+        makeWall(index)
+    
+    position = random.randint(5, MAZE_WIDTH - 5) * index 
+    print(position)
+
+    mazePainter.penup()
+    mazePainter.backward(position) 
     mazePainter.right(90)
     mazePainter.backward(MAZE_WIDTH)
     mazePainter.pendown()
     mazePainter.forward(MAZE_WIDTH)
     mazePainter.left(90)
     mazePainter.penup()
-    mazePainter.forward((MAZE_WIDTH/2 * index) + 35)
+    mazePainter.forward(position) 
     mazePainter.pendown()
-
-def makeWall(index):
-    mazePainter.forward(MAZE_WIDTH * index)
 
 
 def makeMaze():
     for i in range(MAZE_SIZE):
-        makeDoor(i)
+        makeBarrier(i, True)
         mazePainter.left(90)
-        makeDoor(i)
+        makeBarrier(i, True)
         mazePainter.left(90)
 
 makeMaze()
