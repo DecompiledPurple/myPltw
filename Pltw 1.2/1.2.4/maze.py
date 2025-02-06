0b539d8feddbb7694fe4c758f24724adddecb742
marcelo <mcarrillo@seedschoolla.org>
2025-02-06 11:20:42 -0800

    2/6

--- Pltw 1.2/1.2.4/maze.py
+++ Pltw 1.2/1.2.4/maze.py
@@ -3,12 +3,14 @@ import random
 
 screen = turtle.Screen()
 mazePainter = turtle.Turtle()
-mazePainter.width(5)
-mazePainter.speed(5)
+mazePainter.width(4)
+mazePainter.speed(0)
 
-MAZE_SIZE = 15
-MAZE_WIDTH = 35
-DOOR_SIZE = 35
+MIN_MAZE = 4
+MAZE_SIZE = 50
+MAZE_WIDTH = 10
+DOOR_SIZE = 10
+
 
 def makeWall(index):
     mazePainter.forward(MAZE_WIDTH * index)
@@ -16,7 +18,6 @@ def makeWall(index):
 def makeDoor(index):
     position = random.randint(-MAZE_WIDTH, MAZE_WIDTH) * index /2.1
     splitWallSize = MAZE_WIDTH/2 * index - DOOR_SIZE/2
-
 
     mazePainter.forward(splitWallSize + position) #make part of wall
     mazePainter.penup()
@@ -30,7 +31,7 @@ def makeBarrier(index, withDoor):
     else:    
         makeWall(index)
     
-    position = random.randint(5, MAZE_WIDTH - 5) * index 
+    position = random.randint(0, MAZE_WIDTH) * index 
     print(position)
 
     mazePainter.penup()
@@ -47,10 +48,17 @@ def makeBarrier(index, withDoor):
 
 def makeMaze():
     for i in range(MAZE_SIZE):
-        makeBarrier(i, True)
-        mazePainter.left(90)
-        makeBarrier(i, True)
-        mazePainter.left(90)
+        if i >= MIN_MAZE:
+            print("guh")
+            makeBarrier(i, True)
+            mazePainter.left(90)
+            makeBarrier(i, True)
+            mazePainter.left(90)
+        else:
+            makeWall(i)
+            mazePainter.left(90)
+            makeDoor(i)
+            mazePainter.left(90)
 
 makeMaze()
 
